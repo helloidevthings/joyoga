75944ce0776843fafe5353b97d6b838a167ed317
Jo <helloidevthings@gmail.com>
2025-04-30 08:45:56 -0400

    fixing fonts trying tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Joyoga</h1>
-        <h2>See you in class!</h2>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-center">
+        <div className="justify-items-center text-align-center">
+          <h1 className="text-4xl md:text-6xl lg:txt-8xl">Joyoga</h1>
+          <h2 className="text-2xl md:text-4xl lg:txt-6xl">See you in class!</h2>
+        </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
