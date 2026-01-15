1b2c9303ee5f194db4d1b5d68887a15988788e52
helloidevthings <helloidevthings@gmail.com>
2026-01-15 09:39:12 -0500

    updates to theme...

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]" data-theme="coffee">
       <GoogleAnalytics gaId="G-5ZJY5MZT6M" />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-center w-full max-w-3xl mx-auto">
 
