c55fcffbb857cf2802427b993af7ad57f7bd95c3
helloidevthings <helloidevthings@gmail.com>
2026-01-24 10:08:33 -0500

    trying original settings

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -54,7 +54,7 @@ export async function POST(request) {
 
     try {
       await resend.emails.send({
-        from: 'Joyanna <noreply@joyogaflow.com>',
+        from: 'Joyanna <reply@joyogaflow.com>',
         to: 'aquajoybubble@gmail.com',
         subject: `New Sign-Up: ${name} - Pour Taproom Class`,
         text: emailContent,
